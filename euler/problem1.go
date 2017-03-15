b8ae8f8fe50d1fd6bb53a075b3bec89d40a60a22
mmartin <mmartin@greyman.es>
2017-03-15 20:55:23 +0100

    Finished

--- euler/problem1.go
+++ euler/problem1.go
@@ -1,1 +1,24 @@
+package main
+
+import (
+    "fmt"
+)
+
+func isMultiple(number int, divisor int) bool {
+    if number % divisor == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+func main() {
+    sum := 0
+    for n, bound := 3, 1000; n<bound; n++ {
+        if isMultiple(n, 3) || isMultiple(n,5) {
+            sum += n
+        }
+    }
+    fmt.Printf("Sum of multiples of 3 or 5 below 1000 is: %d\n", sum)
+}
 
